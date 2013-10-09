1f883b536ec5d45213a48cae736fe3742c41a007
marianodominguez <mariano.dm@gmail.com>
2013-10-09 10:37:29 -0700

    solved pc14

--- pythonChallenge/src/pc14_italy.py
+++ pythonChallenge/src/pc14_italy.py
@@ -3,32 +3,32 @@ from PIL import Image, ImageDraw
 im = Image.open("wire.png")
 data = im.load()
 
-out = Image.new(im.mode, (100,100))
+out = Image.new(im.mode, (101,101))
 w = 100
 t = 0
-i=0
+i = 0
 
 #spiral
 
-while i<w*w:
+while t<100:
+    #print "turn",t
     y=t
     for x in xrange(t, w-t):
-        out.putpixel((x ,y) , data[i,0])
+        out.putpixel((x,y) , data[i,0])
         i+=1
-    t+=1
-    x=w-t
-    for y in xrange(t, w-t):
+    x=w-t-1
+    for y in xrange(t+1, w-t):
         out.putpixel((x,y), data[i,0])
         i+=1
     y=w-t
-    for x in xrange(w-t , t-1, -1):
+    for x in xrange(w-t-1 , t, -1):
         out.putpixel((x,y), data[i,0])
         i+=1
-    x=t-1
-    t+=1
-    for y in xrange(w-t, t, -1):
+    x=t+1
+    for y in xrange(w-t-1, t+1, -1):
         out.putpixel((x,y), data[i,0])    
         i+=1
+    t+=1
 
 out.save('italy_14.jpg')
 out.show()
