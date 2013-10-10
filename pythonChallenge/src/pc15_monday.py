d6d95ffa44a9615dbec9ec253b0a4893ad48e121
marianodominguez <mariano.dm@gmail.com>
2013-10-10 13:29:18 -0700

    solved challenge 16 partial

--- pythonChallenge/src/pc15_monday.py
+++ pythonChallenge/src/pc15_monday.py
@@ -1,10 +1,10 @@
 import calendar
 
-year = 1906
+year = 1006
 
 while year<2000:
     day = calendar.weekday(year, 1, 26)
-    if day is 0:
+    if day is 0 and calendar.isleap(year):
         print calendar.month(year, 1)
     year +=10
 
---
+++ pythonChallenge/src/pc16_mozart.py
@@ -0,0 +1,34 @@
+import Image
+
+im = Image.open("mozart.gif")
+data = im.load()
+w,h = im.size
+
+result = Image.new(im.mode, (w*2+1, h))
+
+offset = [0]*h
+
+x=0
+y=0
+
+while y<h:
+    marker = False
+    while not marker:
+        pixel = data[x,y]
+        if pixel >= 200 and data[x+1, y] == 195:
+            marker = True
+            offset[y] = x; 
+        x+=1
+    y+=1
+    x=0
+
+#print offset
+
+for y in xrange(h):
+    for x in range(w):
+        nx = x + (w - offset[y])
+        #print nx
+        result.putpixel((nx,y), data[x,y])
+
+result.save('straight.gif')
+result.show()
