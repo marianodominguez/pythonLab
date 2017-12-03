92a9c786dcb755cf98ed412276bc5e8de64184ce
marianodominguez <mariano.dm@gmail.com>
2017-12-03 13:37:01 -0800

    Add walls to maze

--- labs/DisplayMaze.py
+++ labs/DisplayMaze.py
@@ -8,17 +8,6 @@ import pygame
 import sys
 from pygame.locals import *
 
-MAZE_W,MAZE_H = (100,100)
-WALL_COLOR = (100, 30, 100)
-START_COLOR = (0, 255, 0)
-END_COLOR = (255, 0, 0)
-
-mode = width,height = 800,600
-maze = []
-myX, myY = (1,2)
-screen = None
-hscale,vscale=(1.0,1.0)
-
 class Maze(object):
     '''
     Create and display a maze from strings, with the following format
@@ -27,57 +16,84 @@ class Maze(object):
              E\n
     '''
 
+    mode = width,height = 800,600
+    maze = []
+    myX, myY = (1,2)
+    screen = None
+    hscale,vscale=(1.0,1.0)
+    MAZE_W,MAZE_H = (100,100)
+    WALL_COLOR = (100, 30, 100)
+    START_COLOR = (0, 255, 0)
+    END_COLOR = (255, 0, 0)
+
     def __init__(self,params=[]):
         '''
         Constructor
         '''
         
         pygame.init()
-        self.screen = pygame.display.set_mode(mode)
-        self.hscale = (width) / MAZE_W
-        self.vscale = (height) / MAZE_H
+        self.screen = pygame.display.set_mode(self.mode)
+        self.hscale = (self.width) / self.MAZE_W
+        self.vscale = (self.height) / self.MAZE_H
         self.maze = [];
         self.myX, self.myY = (1,1)
         
 
     def display(self):
         self.screen.fill((0,0,0))
-        for y,row in enumerate(self.maze):
-            for x,cell in enumerate(row):
+        for x,row in enumerate(self.maze):
+            for y,cell in enumerate(row):
                 rect = pygame.Rect(x*self.hscale, y*self.vscale, self.hscale, self.vscale)
                 if cell== 'S':
-                    pygame.draw.rect(self.screen, START_COLOR, rect)
+                    pygame.draw.rect(self.screen, self.START_COLOR, rect)
                 if cell== '*':
-                    pygame.draw.rect(self.screen, WALL_COLOR, rect)
+                    pygame.draw.rect(self.screen, self.WALL_COLOR, rect)
                 if cell == 'E':
-                    pygame.draw.rect(self.screen, END_COLOR, rect)
+                    pygame.draw.rect(self.screen, self.END_COLOR, rect)
             
 
-    def setMaze(self, mazeString = [' S ', '   ', ' E ']):
+    def setMaze(self,mazeString = [' S ', '   ', ' E ']):
         for row in mazeString:
             self.maze.append(row)
         self.MAZE_W = len(mazeString[0])
         self.MAZE_H = len(mazeString)
-        self.hscale = (width) / self.MAZE_W
-        self.vscale = (height) / self.MAZE_H
+        self.hscale = (self.width) / self.MAZE_W
+        self.vscale = (self.height) / self.MAZE_H
+        #print self.maze
+
+    def move(self,dir):        
+        if dir=='U':
+            if self.myY-1 >= 0 and self.maze[self.myX][self.myY-1] == ' ' :
+                self.myY -= 1
+        if dir=='D':
+            if self.myY+1 <= self.MAZE_H and self.maze[self.myX][self.myY+1] == ' ':
+                self.myY += 1
+        if dir=='L':
+            if self.myX-1 >= 0 and self.maze[self.myX-1][self.myY] == ' ' :
+                self.myX -= 1
+        if dir=='R':
+            if self.myX+1 <= self.MAZE_W and self.maze[self.myX+1][self.myY] == ' ':
+                self.myX += 1
 
     def game(self):
         while True:
-            rect = pygame.Rect(self.myX*self.hscale, self.myY*self.vscale, self.hscale, self.vscale)
+            rect = pygame.Rect(self.myX*self.hscale,self.myY*self.vscale,self.hscale,self.vscale)
             pygame.draw.rect(self.screen, (0,0,0), rect)
+ 
             for event in pygame.event.get():
                 if event.type == QUIT:
                     sys.exit()
-                if event.type == KEYDOWN:    
+                if event.type == KEYDOWN:
                     if event.key == pygame.K_UP:
-                        if self.myY > 0: self.myY -= 1
+                        self.move("U")
                     if event.key == pygame.K_DOWN:
-                        if self.myY < MAZE_H : self.myY += 1
+                        self.move("D")
                     if event.key == pygame.K_LEFT:
-                        if self.myX > 0: self.myX -= 1
+                        self.move("L")
                     if event.key == pygame.K_RIGHT:
-                        if self.myX < MAZE_W: self.myX += 1
-                rect = pygame.Rect(self.myX*self.hscale, self.myY*self.vscale, self.hscale, self.vscale)
+                        self.move("R")
+                
+                rect = pygame.Rect(self.myX*self.hscale,self.myY*self.vscale,self.hscale,self.vscale)
                 pygame.draw.rect(self.screen, (255,255,255), rect)
                 pygame.display.update()
                  
